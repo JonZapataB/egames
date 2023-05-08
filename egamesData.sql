e27b779f0a887241e0f733860da920648c77c90b
Jon <jonzapatab@gmail.comgit config --global user.name JonZapataB>
2023-05-08 17:16:00 +0200

    tablas mysql

---
+++ egamesData.sql
@@ -0,0 +1,38 @@
+INSERT INTO games (name, description, release_date) VALUES
+('Super Mario Odyssey', 'Únete a Mario en una emocionante aventura para rescatar a la Princesa Peach de las garras de Bowser. Explora diversos reinos y disfruta de la jugabilidad innovadora de Nintendo Switch.', '2017-10-27'),
+('The Legend of Zelda: Breath of the Wild', 'Embárcate en una épica aventura en el vasto reino de Hyrule mientras te enfrentas a desafiantes enemigos y resuelves puzles. Descubre un mundo abierto lleno de secretos y maravillas.', '2017-03-03'),
+('Animal Crossing: New Horizons', 'Crea tu propia isla paradisíaca y vive una vida virtual llena de diversión y amistad. Construye tu hogar, haz amigos, pesca, atrapa insectos y disfruta de eventos estacionales.', '2020-03-20'),
+('Mario Kart 8 Deluxe', 'Únete a Mario y sus amigos en emocionantes carreras llenas de acción. Compite en pistas clásicas o nuevas, solo o con amigos en modo multijugador local o en línea.', '2017-04-28'),
+('Splatoon 2', 'Sumérgete en batallas de tinta multijugador en línea y enfréntate a equipos rivales en emocionantes modos de juego. Forma parte del equipo de los Inkling y demuestra tu habilidad en el combate.', '2017-07-21'),
+('Pokémon Sword', 'Embárcate en una aventura Pokémon en la región de Galar. Captura, entrena y combate con Pokémon mientras te conviertes en el campeón de la Liga Pokémon.', '2019-11-15'),
+('Fire Emblem: Three Houses', 'Sumérgete en un mundo de intriga y batallas estratégicas. Elige tu casa y forja relaciones con personajes únicos mientras tomas decisiones que afectarán el destino de Fódlan.', '2019-07-26'),
+('Luigi\'s Mansion 3', 'Ayuda a Luigi a rescatar a sus amigos atrapados en el Hotel Último Destino, donde se enfrentará a fantasmas y resolverá puzles.', '2019-10-31'),
+('Super Smash Bros. Ultimate', 'Reúne a los personajes más icónicos de Nintendo y lucha en emocionantes combates.', '2018-12-07'),
+('Xenoblade Chronicles 2', 'Descubre un vasto mundo lleno de misterios y peligros. Únete a Rex y su compañera Pyra en una búsqueda para encontrar el Paraíso de Elysium en esta épica aventura de rol.', '2017-12-01'),
+('Splatoon 3', 'Vuelve a sumergirte en emocionantes batallas de tinta en esta tercera entrega de la exitosa saga Splatoon.', '2023-06-30'),
+('Metroid Prime 4', 'Únete a Samus Aran en su nueva misión en la saga Metroid Prime, explorando planetas y combatiendo a peligrosos enemigos.', '2023-09-15'),
+('Super Mario Party', 'Disfruta de divertidos minijuegos y compite con tus amigos en este juego de fiesta con temática de Mario.', '2018-10-05'),
+('The Legend of Zelda: Link s Awakening', 'Revive la clásica aventura de Link en una isla misteriosa llena de desafíos y secretos por descubrir.', '2019-09-20'),
+('Mario Tennis Aces', 'Participa en emocionantes partidos de tenis con personajes de Mario mientras desbloqueas movimientos especiales y enfrentas desafíos.', '2018-06-22'),
+('Pokémon Let\'s Go, Pikachu!', 'Embárcate en una aventura junto a Pikachu en esta recreación de la región de Kanto. Captura Pokémon, lucha contra líderes de gimnasio y desafía a la Liga Pokémon.', '2018-11-16'),
+('Super Mario Maker 2', 'Crea y juega tus propios niveles de Super Mario en este divertido juego de construcción y plataformas.', '2019-06-28'),
+('Donkey Kong Country: Tropical Freeze', 'Acompaña a Donkey Kong y sus amigos en una aventura llena de plataformas y desafíos en una isla tropical.', '2018-05-04'),
+('Splatoon 2: Octo Expansion', 'Explora nuevas misiones y desafíos en esta expansión para Splatoon 2, donde juegas como un Inkling Octariano.', '2018-06-13'),
+('Fire Emblem: Three Houses - Expansion Pass', 'Amplía tu experiencia en Fire Emblem: Three Houses con contenido adicional, incluyendo nuevas historias y personajes.', '2019-07-26'),
+('Yoshi\'s Crafted World', 'Acompaña a Yoshi en una aventura llena de color y creatividad en un mundo hecho de papel y cartón.', '2019-03-29'),
+('Kirby Star Allies', 'Únete a Kirby y sus amigos en un viaje para salvar Dream Land de una fuerza maligna en esta colorida aventura de plataformas.', '2018-03-16'),
+('New Super Mario Bros. U Deluxe', 'Disfruta de la clásica acción de plataformas de Mario en esta versión mejorada con nuevos personajes y desafíos.', '2019-01-11'),
+('The Legend of Zelda: Skyward Sword HD', 'Revive la legendaria historia de Link en su búsqueda por rescatar a Zelda en esta versión remasterizada para Nintendo Switch.', '2021-07-16'),
+('Pokémon Shield', 'Explora la región de Galar y descubre nuevas especies de Pokémon mientras te enfrentas a desafiantes entrenadores y participas en la Liga Pokémon.', '2019-11-15'),
+('The Witcher 3: Wild Hunt - Complete Edition', 'Únete a Geralt de Rivia en su búsqueda por encontrar a Ciri y enfrentar a los peligrosos monstruos y fuerzas oscuras en este juego de rol épico de mundo abierto.', '2019-10-15'),
+('Hades', 'Embárcate en una aventura infernal para escapar del inframundo mientras enfrentas a los dioses del Olimpo. Descubre nuevos objetos y habilidades en cada intento.', '2020-09-12'),
+('Octopath Traveler', 'Sumérgete en un mundo de fantasía y descubre las historias de ocho personajes únicos. Combate en emocionantes batallas por turnos mientras exploras un mundo vasto y detallado.', '2018-07-13'),
+('Celeste', 'Ayuda a Madeline en su viaje de autodescubrimiento mientras escalas la montaña Celeste. Enfrenta desafiantes obstáculos y supera tus propios límites en este emocionante juego de plataformas.', '2018-01-25'),
+('Mario + Rabbids Kingdom Battle', 'Únete a Mario, Luigi, Peach y los Rabbids en una emocionante aventura para restaurar el Reino Champiñón. Combate en emocionantes batallas tácticas y resuelve puzles desafiantes.', '2017-08-29'),
+('Dragon Quest XI S: Echoes of an Elusive Age - Definitive Edition', 'Únete a un joven héroe en su búsqueda por descubrir su verdadero destino y enfrentar el mal que amenaza al mundo. Descubre un vasto mundo lleno de personajes coloridos y combate en emocionantes batallas por turnos.', '2019-09-27'),
+('Stardew Valley', 'Escapa de la vida urbana y comienza una nueva vida en tu propia granja. Cultiva tus propios cultivos, cuida animales y forma relaciones con los habitantes del pueblo en este juego de simulación relajante.', '2017-10-05');
+
+INSERT INTO stock (idgame, stock, platform, price)
+SELECT idgame, 100, 'Nintendo Switch', 3000
+FROM games
+WHERE idgame BETWEEN 1 AND 32;
